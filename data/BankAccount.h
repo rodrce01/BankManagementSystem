20be546e798e1a1a9e909980958db9180ca5aecc
ajoh2022-source <ajoh.2022@gmail.com>
2025-12-08 21:55:50 -0600

    Update BankAccount.h

--- data/BankAccount.h
+++ data/BankAccount.h
@@ -1,39 +1,31 @@
 #ifndef BANKACCOUNT_H
 #define BANKACCOUNT_H
-
 #include <string>
-#include <vector>
 
 class BankAccount {
 private:
     int accountNumber;
     std::string accountType;
     double balance;
-    std::vector<std::string> transactionHistory;
 
-    static int accountCount; 
+    static int totalAccounts; 
 
 public:
     BankAccount();  
-    BankAccount(int accNum, const std::string& type, double bal);
-    BankAccount(const BankAccount& other);
-
-    ~BankAccount();
+    BankAccount(int number, const std::string &type, double bal);
+    BankAccount(const BankAccount &other);
 
     int getAccountNumber() const;
-    void setAccountNumber(int accNum);
-
     std::string getAccountType() const;
-    void setAccountType(const std::string& type);
-
     double getBalance() const;
-    void setBalance(double bal);
 
+    void setAccountType(const std:: string &type);
+
+    bool withDraw(double amount);
     void deposit(double amount);
-    bool withdraw(double amount);
     void printAccountSummary() const;
 
-    static int getAccountCount();
+    static int getTotalAccounts();
 };
 
 #endif
